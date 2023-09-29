f7bf8e6e5d3bb67fd79d571ebb4c992943f74728
Killian LE VAILLANT <killian.levaillant@gmail.com>
2023-09-29 14:03:14 +0200

    vm generator

--- main.js
+++ main.js
@@ -1,30 +1,57 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const { spawn } = require('child_process');
+
+let mainWindow;
 
 function createWindow () {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  mainWindow.loadFile('index.html');
+
+  // Attendre que la fenêtre soit prête pour lancer la commande
+  mainWindow.webContents.on('did-finish-load', () => {
+    // Lorsque la fenêtre est prête, vous pouvez activer la gestion de la commande
+    ipcMain.on('start-command', (event, data) => {
+      const { repoURL, launchCommand } = data;
+
+      // Exécutez la commande avec child_process.spawn
+      const childProcess = spawn('vmg', ['--', '-r', repoURL, '-c', launchCommand]);
+
+      // Rediriger la sortie de la commande vers la fenêtre Electron
+      childProcess.stdout.on('data', (data) => {
+        mainWindow.webContents.send('command-output', data.toString());
+      });
+
+      childProcess.stderr.on('data', (data) => {
+        mainWindow.webContents.send('command-error', data.toString());
+      });
+
+      childProcess.on('close', (code) => {
+        mainWindow.webContents.send('command-exit', code);
+      });
+    });
+  });
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
--- preload.js
+++ preload.js
@@ -1,10 +1,38 @@
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
   for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+    replaceText(`${type}-version`, process.versions[type]);
   }
-})
+
+  // Importez ipcRenderer ici
+  const { ipcRenderer } = require('electron');
+
+  const startButton = document.getElementById('start-button');
+  const repoInput = document.getElementById('repo-input');
+  const commandInput = document.getElementById('command-input');
+  const commandOutput = document.getElementById('command-output');
+
+  startButton.addEventListener('click', () => {
+    const repoURL = repoInput.value;
+    const launchCommand = commandInput.value;
+
+    // Envoyer les données au processus principal
+    ipcRenderer.send('start-command', { repoURL, launchCommand });
+  });
+
+  ipcRenderer.on('command-output', (event, data) => {
+    commandOutput.innerText += data;
+  });
+
+  ipcRenderer.on('command-error', (event, data) => {
+    commandOutput.innerText += `Erreur: ${data}`;
+  });
+
+  ipcRenderer.on('command-exit', (event, code) => {
+    commandOutput.innerText += `La commande a quitté avec le code ${code}`;
+  });
+});
